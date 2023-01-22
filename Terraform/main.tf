e9c029e86abbf1b2ccfb75f29a055de389967029
Venky0827 <bandaruvenkatesh0827@gmail.com>
2023-01-23 00:21:09 +0530

    Modified files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,14 +1,23 @@
 provider "aws" {
-    access_key = "xxxxxxxxxxxxxxxxxxxxxx"
-    secret_key = "xxxxxxxxxxxxxxxxxxxxx"
+    access_key = var.access_key
+    secret_key = var.secret_key
     region = "us-east-1"
 }
 
-resource "aws_vpc" "default" {
-    cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "VPC1" {
+    cidr_block = var.cidr_block1
     enable_dns_hostnames = true
     tags = {
-        Name = "terraform_vpc"
+        Name = "vpc_name1"
+        Owner = "Venkatesh Bandaru"
+    }
+}
+
+resource "aws_vpc" "VPC2" {
+    cidr_block = var.cidr_block2
+    enable_dns_hostnames = true
+    tags = {
+        Name = "vpc_name2"
         Owner = "Venkatesh Bandaru"
     }
 }
---
+++ Terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "vpc_name1" {}
+variable "cidr_block1" {}
+variable "vpc_name2" {}
+variable "cidr_block2" {}
+variable "access_key" {}
+variable "secret_key" {}
